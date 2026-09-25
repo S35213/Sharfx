b5974f87a45bee783d13575cbd837c396f1f24d3
S35213 <150sharingan2@gmail.com>
2026-09-25 16:28:59 +0300

    Keep SHARFX intro visible long enough to see logo

--- src/components/account/PublicWelcome.tsx
+++ src/components/account/PublicWelcome.tsx
@@ -30,7 +30,7 @@ export const PublicWelcome: React.FC = () => {
   })
 
   useEffect(() => {
-    const timer = window.setTimeout(() => setBrandIntroVisible(false), 720)
+    const timer = window.setTimeout(() => setBrandIntroVisible(false), 1400)
     return () => window.clearTimeout(timer)
   }, [])
   const [showAuth, setShowAuth] = useState(false)
