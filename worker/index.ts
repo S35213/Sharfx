89de46fde0346edfd2a1dc97876f9e4de33a5707
S35213 <150sharingan2@gmail.com>
2026-09-26 14:47:13 +0300

    fix: proxy live market data through supported Deriv websocket

--- worker/index.ts
+++ worker/index.ts
@@ -41,7 +41,7 @@ const handleDerivPublicMarketWebSocket = async (request: Request): Promise<Respo
   const server = pair[1]
   server.accept()
 
-  const upstreamResponse = await fetch('https://api.derivws.com/trading/v1/options/ws/public', {
+  const upstreamResponse = await fetch('https://ws.binaryws.com/websockets/v3', {
     headers: { Upgrade: 'websocket' },
   })
   const upstream = upstreamResponse.webSocket
